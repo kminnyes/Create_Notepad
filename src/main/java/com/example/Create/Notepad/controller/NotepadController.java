0f756bac3d1499108a3189fb6aa5003c18b72e72
Kyongmin Yea <kminnyes@gmail.com>
2024-02-14 21:04:18 +0900

    메모 읽기, 수정, 삭제

--- src/main/java/com/example/Create/Notepad/controller/NotepadController.java
+++ src/main/java/com/example/Create/Notepad/controller/NotepadController.java
@@ -7,9 +7,13 @@ import com.example.Create.Notepad.repository.NotepadRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -33,7 +37,61 @@ public class NotepadController {
         // 리포지토리로 엔티티를 DB에 저장
         Notepad saved = notepadRepository.save(notepad);
         log.info(saved.toString());
-        return "";
+        return "redirect:/notepad/" + saved.getId();
     }
 
+    @GetMapping("/notepad/{id}")
+    public String detail(@PathVariable Long id, Model model){
+        log.info("id =" + id);
+        //id 조회 데이터 가져오기
+        Notepad notepadEntity = notepadRepository.findById(id).orElse(null);
+        // 모델에 데이터 등록
+        model.addAttribute("notepad", notepadEntity);
+        // 뷰 페이지 반환
+        return "notepad/detail";
+    }
+
+    @GetMapping("/notepad")
+    public String index(Model model){
+        ArrayList<Notepad> notepadEntityList = notepadRepository.findAll();
+        model.addAttribute("notepadList", notepadEntityList);
+        return "notepad/index";
+    }
+
+    @GetMapping("/notepad/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        // 수정할 데이터 가져오기
+        Notepad notepadEntity = notepadRepository.findById(id).orElse(null);
+        //데이터 등록
+        model.addAttribute("notepad", notepadEntity);
+        return "notepad/edit";
+    }
+
+    @PostMapping("/notepad/update")
+    public String update(NotepadForm form){
+        log.info(form.toString());
+        Notepad notepadEntity = form.toEntity();
+        log.info(form.toString());
+
+        Notepad target = notepadRepository.findById(notepadEntity.getId()).orElse(null);
+        if(target != null){
+            notepadRepository.save(notepadEntity);
+        }
+        return "redirect:/notepad/" + notepadEntity.getId();
+    }
+
+    @GetMapping("/notepad/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes tr){
+        log.info("삭제 요청!");
+        Notepad target = notepadRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        //엔티티 삭제
+        if(target != null){
+            notepadRepository.delete(target);
+            tr.addFlashAttribute("msg", "삭제되었습니다.");
+        }
+
+        return "redirect:/notepad";
+    }
 }
--- src/main/java/com/example/Create/Notepad/repository/NotepadRepository.java
+++ src/main/java/com/example/Create/Notepad/repository/NotepadRepository.java
@@ -3,5 +3,9 @@ package com.example.Create.Notepad.repository;
 import com.example.Create.Notepad.entity.Notepad;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface NotepadRepository extends CrudRepository<Notepad, Long> {
+    @Override
+    ArrayList<Notepad> findAll(); // Iterable -> ArrayList 로 수정
 }
